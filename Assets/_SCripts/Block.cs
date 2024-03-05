f71fb3fd5795d6c1cf7c4c0b7fc40d064560cffb
CoreyMeyerCin <72515852+CoreyMeyerCin@users.noreply.github.com>
2024-03-05 17:12:20 -0500

    Fixed Double Hitting/ Added Corrupted Blocks
    Added a timer to stop double hits, and added Corruption Blocks.

--- Assets/_SCripts/Block.cs
+++ Assets/_SCripts/Block.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+    public enum BlockType{
+        Normal,
+        Corrupted
+    }
 public class Block : MonoBehaviour
 {
+    
+    private float hitTimer = 0f;
+    private float hitCooldown = 0.1f;
     public int hitsToBreak;
     public int points = 10;
     public GameObject blockText;
     public List<Color> colors;
     public List<SpriteRenderer> spriteRenderers;
     public int colorIndex = 0;
+    public BlockType blockType;
+
     void Awake()
     {
-        hitsToBreak = Random.Range(1, 7);
+        hitsToBreak = Random.Range(1, colors.Count+1);
         colorIndex = hitsToBreak-1;
         blockText.GetComponent<TextMesh>().text = hitsToBreak.ToString();
         foreach(SpriteRenderer sr in spriteRenderers)
@@ -20,18 +29,38 @@ public class Block : MonoBehaviour
             Color opaqueColor = new Color(colors[colorIndex].r, colors[colorIndex].g, colors[colorIndex].b, 1f);
             sr.color = opaqueColor;
         }
+        if(blockType == BlockType.Corrupted){
+            ScoreManager.Instance.CorruptedBlocks++;
+            ScoreManager.Instance.UpdateScoreText();
+        }
+    }
+
+    void Update()
+    {
+        if (hitTimer > 0)
+        {
+            hitTimer -= Time.deltaTime;
+        }
     }
 
     public void HitBlock()
     {
+        if (hitTimer > 0)
+        {
+            return;
+        }
+        hitTimer = hitCooldown;
         ScoreManager.Instance.AddPoints(points);
         hitsToBreak--;
         colorIndex--;
         Debug.Log("hitsToBreak: " + hitsToBreak);
         StartCoroutine(Camera.main.GetComponent<CameraShake>().Shake(0.06f, 0.06f));
         if(hitsToBreak <= 0){
-            Debug.Log("Destroying block");
-            ScoreManager.Instance.Blocks--;
+            if(blockType == BlockType.Corrupted){
+                Debug.Log("Destroying block");
+                ScoreManager.Instance.CorruptedBlocks--;
+                Debug.Log("Corrupted Block Count::" + ScoreManager.Instance.CorruptedBlocks);
+            }
             ScoreManager.Instance.UpdateScoreText();
             Destroy(gameObject);
         }
@@ -41,5 +70,6 @@ public class Block : MonoBehaviour
             Color opaqueColor = new Color(colors[colorIndex].r, colors[colorIndex].g, colors[colorIndex].b, 1f);
             sr.color = opaqueColor;
         }
+
     }
 }
